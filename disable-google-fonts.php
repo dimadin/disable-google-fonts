87875398bd1d399ee0f8c513be4b94268c972240
Milan Dinić <forumi@srpski.biz>
2015-12-09 10:40:40 +0100

    Remove fonts used in Twenty Sixteen.

--- disable-google-fonts.php
+++ disable-google-fonts.php
@@ -211,6 +211,46 @@ class Disable_Google_Fonts {
 	}
 
 	/**
+	 * Force 'off' as a result of Merriweather font toggler string translation.
+	 *
+	 * @since 1.2
+	 * @access public
+	 *
+	 * @param  string $translations Translated text.
+	 * @param  string $text         Text to translate.
+	 * @param  string $context      Context information for the translators.
+	 * @param  string $domain       Text domain. Unique identifier for retrieving translated strings.
+	 * @return string $translations Translated text.
+	 */
+	public function disable_merriweather( $translations, $text, $context, $domain ) {
+		if ( 'Merriweather font: on or off' == $context && 'on' == $text ) {
+			$translations = 'off';
+		}
+
+		return $translations;
+	}
+
+	/**
+	 * Force 'off' as a result of Montserrat font toggler string translation.
+	 *
+	 * @since 1.2
+	 * @access public
+	 *
+	 * @param  string $translations Translated text.
+	 * @param  string $text         Text to translate.
+	 * @param  string $context      Context information for the translators.
+	 * @param  string $domain       Text domain. Unique identifier for retrieving translated strings.
+	 * @return string $translations Translated text.
+	 */
+	public function disable_montserrat( $translations, $text, $context, $domain ) {
+		if ( 'Montserrat font: on or off' == $context && 'on' == $text ) {
+			$translations = 'off';
+		}
+
+		return $translations;
+	}
+
+	/**
 	 * Register filters that disable fonts for bundled themes.
 	 *
 	 * This filters can be directly hooked as Disable_Google_Fonts::disable_open_sans()
@@ -233,6 +273,11 @@ class Disable_Google_Fonts {
 		$template = get_template();
 
 		switch ( $template ) {
+			case 'twentysixteen' :
+				add_filter( 'gettext_with_context', array( $this, 'disable_merriweather'    ), 888, 4 );
+				add_filter( 'gettext_with_context', array( $this, 'disable_montserrat'      ), 888, 4 );
+				add_filter( 'gettext_with_context', array( $this, 'disable_inconsolata'     ), 888, 4 );
+				break;
 			case 'twentyfifteen' :
 				add_filter( 'gettext_with_context', array( $this, 'disable_noto_sans'       ), 888, 4 );
 				add_filter( 'gettext_with_context', array( $this, 'disable_noto_serif'      ), 888, 4 );
